27f6a3d83943c4d128af8afacd1471810dde961f
Nolan J Tait <nolanjtait@gmail.com>
2025-05-26 14:48:49 -0700

    Adds initial content

---
+++ content/find-routes.md
@@ -0,0 +1,7 @@
+# Fuzzy finding rails routes
+
+Use `fzf` to quickly search for through your rails routes
+
+```
+bin/rails routes | fzf
+```
--- lib/miyagi.rb
+++ lib/miyagi.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require "tty-markdown"
+
 require_relative "miyagi/version"
 
 module Miyagi
   class Error < StandardError; end
 
   def self.teach
+    file = Pathname.glob("content/**/*.md").sample
+    TTY::Markdown.parse_file(file.to_s, width: 80)
   end
 end
